96edb5af2d7fae6c2aaca636a36063b0abf9882a
Krzysztof Cisło <48331541+wolve265@users.noreply.github.com>
2022-03-28 22:50:43 +0200

    src: L289NMotorDriver: added driver with test (#12)

--- BluetoothCar.h
+++ BluetoothCar.h
@@ -1,7 +1,17 @@
 #ifndef BLUETOOTHCAR_H
 #define BLUETOOTHCAR_H
 
-#include "src/L298N/L298N.h"
-#include "src/L298N/L298N.cpp"
+#define L298N_ENA 5
+#define L298N_IN1 6
+#define L298N_IN2 7
+#define L298N_IN3 8
+#define L298N_IN4 9
+#define L298N_ENB 10
+
+#include "src/L298NMotorDriver/L298NMotorDriver.h"
+#include "src/L298NMotorDriver/L298NMotorDriver.cpp"
+
+L298NMotorDriver rightMotorDriver(true, true, L298N_ENA, L298N_IN1, L298N_IN2);
+L298NMotorDriver leftMotorDriver(true, true, L298N_ENB, L298N_IN3, L298N_IN4);
 
 #endif
--- src/MotorDriver/MotorDriver.cpp
+++
@@ -1,9 +0,0 @@
-#include "MotorDriver.h"
-
-MotorDriver::MotorDriver(bool isDebugOn, bool isEnabled) : Driver(isDebugOn, isEnabled)
-{
-}
-
-MotorDriver::~MotorDriver()
-{
-}

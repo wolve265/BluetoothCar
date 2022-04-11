eb6d7ad25daa1189e6a86b5ceabdab480ec690c2
Krzysztof Cisło <48331541+wolve265@users.noreply.github.com>
2022-04-11 13:18:44 +0200

    src: bluetoothSteering: added class with test (#22)

---
+++ src/BluetoothSteering/BluetoothSteering.h
@@ -0,0 +1,29 @@
+#ifndef BLUETOOTH_STEERING_H
+#define BLUETOOTH_STEERING_H
+
+#include "src/CommandDecoder/CommandDecoder.h"
+#include "src/SimpleSteering/SimpleSteering.h"
+
+class BluetoothSteering : public SimpleSteering
+{
+private:
+    CommandDecoder* _commandDecoder;
+    int _minSpeed = 150;
+    int _maxSpeed = 250;
+    int _currentSpeed = _minSpeed;
+
+public:
+    BluetoothSteering(
+        bool isDebugOn,
+        bool isEnabled,
+        L298NMotorDriver* rightMotorDriver,
+        L298NMotorDriver* leftMotorDriver,
+        CommandDecoder* commandDecoder
+        );
+    ~BluetoothSteering();
+
+    void run();
+    void setSpeed(int speed);
+};
+
+#endif
